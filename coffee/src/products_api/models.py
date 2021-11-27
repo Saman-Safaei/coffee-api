03707660e9de5eb0622ad7c40eada525ae09ec18
SamuelSam-dev <xsamansafaeix@gmail.com>
2021-11-27 11:34:26 +0330

    Mass

--- coffee/src/products_api/models.py
+++ coffee/src/products_api/models.py
@@ -3,8 +3,8 @@ from coffee.src.core.extensions import db
 
 class MProducts(db.Model):
     _id = db.Column(db.INTEGER(), primary_key=True, autoincrement=True)
-    name = db.Column(db.VARCHAR(80), nullable=False)
-    image_url = db.Column(db.VARCHAR(), nullable=False)
+    name = db.Column(db.TEXT(), nullable=False)
+    image_url = db.Column(db.TEXT(), nullable=False)
     description = db.Column(db.TEXT(), nullable=False)
     price = db.Column(db.INTEGER(), nullable=False)
     discount = db.Column(db.INTEGER(), nullable=False, default=False)
