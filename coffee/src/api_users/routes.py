19c1a783ea466cca444c1d23017789ffd0ca5886
SamuelSam-dev <xsamansafaeix@gmail.com>
2021-12-02 14:03:38 +0330

    Mass

--- coffee/src/api_users/routes.py
+++ coffee/src/api_users/routes.py
@@ -2,3 +2,4 @@ from . import users_bp
 from . import views
 
 users_bp.add_url_rule("/register", view_func=views.VRegister.as_view("register"))
+users_bp.add_url_rule("/login", view_func=views.VLogin.as_view("login"))
